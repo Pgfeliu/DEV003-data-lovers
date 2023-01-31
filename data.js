b1d177bcaf4decf8532825b0c9ab35445d42f758
Pgfeliu <pamela.gfeliu@gmail.com>
2023-01-31 11:20:04 -0300

    Updates

---
+++ data.js
@@ -0,0 +1,39 @@
+export const genero = (data,gender) => {
+  const datosGenero = data.characters.filter(characters => (characters.gender === gender));
+  return datosGenero
+}
+export function sortMe(data) {
+  const dataOrdenadaAZ = data.sort((a, b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if(nameA < nameB){
+      return -1;
+    }
+    if(nameA > nameB){
+      return 1;
+    }
+    return 0;
+  });
+  return dataOrdenadaAZ
+}
+export function sortMe2(data) {
+  const dataOrdenadaZA = data.sort((a, b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if(nameA < nameB){
+      return 1;
+    }
+    if(nameA > nameB){
+      return -1;
+    }
+    return 0;
+  });
+  return dataOrdenadaZA
+}
+
+
+// Filtro para página hechizos
+export const hechizos = (data,spell_type) => {
+  const datosSpells = data.spells.filter(spells => (spells.spell_type === spell_type));
+  return datosSpells
+   }
